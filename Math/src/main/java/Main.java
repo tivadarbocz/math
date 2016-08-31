07605179beb306f9b9901dd5a07d6572355346d8
bocz.tivadar@lanoga.hu <bocz.tivadar@lanoga.hu>
2016-08-31 11:26:24 +0200

    #1

---
+++ Math/src/main/java/Main.java
@@ -0,0 +1,22 @@
+/**
+ * Created by Tivadar Bocz on 2016.08.31..
+ */
+public class Main {
+    public static void main(String[] args) {
+        int mtx[][] = {
+                {1, 2, -3},
+                {4, -2, 5}
+        };
+
+        int mtx2[][] = {
+                {5, 3, 7},
+                {3, 1, -8}
+        };
+
+        Matrix.printMatrix(mtx);
+        Matrix.printMatrix(mtx2);
+        //Matrix.printMatrix(Matrix.transposition(mtx));
+        //Matrix.printMatrix(Matrix.scalarMultiplication(mtx, 2));
+        //Matrix.printMatrix(Matrix.sum(mtx,mtx2));
+    }
+}
---
+++ Math/src/main/java/Matrix.java
@@ -0,0 +1,99 @@
+/**
+ * Created by Tivadar Bocz on 2016.08.31..
+ */
+public class Matrix {
+    /**
+     *
+     * @return (AT)i,j = Aj,i.
+     */
+    public static int[][] transposition(int[][] mtx){
+        int[][] tmpMatrix = new int[mtx[0].length][mtx.length];
+
+        for(int i = 0; i < mtx.length; ++i){
+            for(int j = 0; j < mtx[0].length; ++j){
+                tmpMatrix[j][i] = mtx[i][j];
+            }
+        }
+        return tmpMatrix;
+    }
+
+    /**
+     *
+     * @param mtx n*m matrix
+     * @param value value of the multiplication
+     * @return (cA)i,j = c · Ai,j.   The product cA of a number c (also called a scalar in the parlance of abstract algebra) and a matrix A is computed by multiplying every entry of A by c:
+    ,
+     */
+    public static int[][] scalarMultiplication(int[][] mtx, int value){
+        for(int i = 0; i < mtx.length; ++i){
+            for(int j = 0; j < mtx[0].length; ++j){
+                mtx[i][j] =  mtx[i][j] * value;
+            }
+        }
+        return mtx;
+    }
+
+    /**
+     *
+     * @param a n*m matrix
+     * @param b n*m matrix
+     * @return with new n*m matrix after addition
+     */
+    public static int[][] sum(int[][] a, int[][] b){
+        if(a.length != b.length || a[0].length != b[0].length){
+            throw new IllegalArgumentException("This method excepts only 2 n*m matrix!");
+        }
+        else{
+            int[][] mtx = new int[a.length][a[0].length];
+            for(int i = 0; i < a.length; ++i){
+                for(int j = 0; j < a[0].length; ++j){
+                    mtx[i][j] = a[i][j] + b[i][j];
+                }
+            }
+            return mtx;
+        }
+    }
+
+    public static void printMatrix(int[][] mtx){
+        for(int i = 0; i < mtx.length; ++i){
+            for(int j = 0; j < mtx[0].length; ++j){
+                System.out.print(mtx[i][j] + "\t"/*makeSpace(String.valueOf(mtx[i][j]).length())*/);
+            }
+            System.out.print(System.lineSeparator());
+        }
+    }
+
+    public String makeSpace(int n){
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < n; ++i){
+            sb.append(" ");
+        }
+
+        return sb.toString();
+    }
+
+    /**
+     *
+     * @param a n * m matrix
+     * @param b n * m matrix
+     * @return true if all values are equal, false if values are not equal
+     */
+    static boolean isEqual(int[][] a, int[][] b){
+        if(a.length != b.length || a[0].length != b[0].length){
+            throw new IllegalArgumentException("This method excepts only 2 n*m matrix!");
+        }
+        else{
+            for(int i = 0; i < a.length; ++i){
+                for(int j = 0; j < a[0].length; ++j){
+                   if(!(a[i][j] == b[i][j])){
+                       return false;
+                   }
+                }
+            }
+        }
+        return true;
+    }
+    //boolean isNotEqual
+    //boolean isLesser
+    //boolean isLesserOrEqual
+}
